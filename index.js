cc678ebb86af45bac468812e9b3fb0708455fca1
Derek Smyth <smyth.derek@gmail.com>
2023-02-01 19:28:44 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(array) {
+    return array.slice(0,2);
+}
+
+returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']);
+
+const returnLastTwoDrivers = function(array) {
+    return array.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+    return function fareMultiplier(fare) {
+        return (fare * integer);
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function selectDifferentDrivers(array, functionName) {
+    return functionName(array);
+}
+
+selectDifferentDrivers(drivers, returnFirstTwoDrivers);
+
+selectDifferentDrivers(drivers, returnLastTwoDrivers);
